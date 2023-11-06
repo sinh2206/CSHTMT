84fb0f549007652434b83cfda7bf4e90297c6a69
sinh2206 <127322500+sinh2206@users.noreply.github.com>
2023-11-06 09:20:54 +0700

    2

---
+++ EDD.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+struct Exercise_01
+{
+    /* data */
+    int s;
+    int f;
+    int d;
+};
+int main() {
+    struct Exercise_01 a1,a2,a3;
+    a1.s = 1,a1.f = 2;
+    printf("%d\n",a1.s);
+    printf("%d",a1.f);
+}
--- Exercise_01.c
+++ Exercise_01.c
@@ -1,14 +1,31 @@
 #include <stdio.h>
-struct Exercise_01
-{
-    /* data */
-    int s;
-    int f;
-    int d;
+
+// Struct đại diện cho một tác vụ
+struct Task {
+    int id;        // ID của tác vụ
+    int deadline;  // Deadline của tác vụ
+    int executionTime;  // Thời gian thực hiện của tác vụ
 };
+
 int main() {
-    struct Exercise_01 a1;
-    a1.s = 1,a1.f = 2;
-    printf("%d\n",a1.s);
-    printf("%d",a1.f);
+    struct Task tasks[] = {
+        {1, 15, 1},
+        {2, 11, 2},
+        {3, 18, 3},
+        {4, 16, 4},
+        {5, 20, 5},
+        {6, 20, 6},
+        {7, 20, 7}
+    };
+    int n = sizeof(tasks) / sizeof(tasks[0]);
+    printf(sizeof(tasks),"\n");
+    printf("\n",sizeof(tasks[0]));
+    printf("ID  | Deadline | Execution Time\n");
+    printf("-------------------------------\n");
+
+    for (int i = 0; i < n; i++) {
+        printf("%2d  |    %2d    |      %2d\n", tasks[i].id, tasks[i].deadline, tasks[i].executionTime);
+    }
+
+    return 0;
 }
